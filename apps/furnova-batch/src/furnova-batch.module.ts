aa402b3521d30c03c18e59bd142a8d0d22053ae0
Abdulloh2505 <daikarnon.5002@gmail.com>
2026-03-04 00:22:02 +0900

    feat: configure env variables

--- apps/furnova-batch/src/furnova-batch.module.ts
+++ apps/furnova-batch/src/furnova-batch.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FurnovaBatchController } from './furnova-batch.controller';
 import { FurnovaBatchService } from './furnova-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+     ConfigModule.forRoot()
+  ],
   controllers: [FurnovaBatchController],
   providers: [FurnovaBatchService],
 })
