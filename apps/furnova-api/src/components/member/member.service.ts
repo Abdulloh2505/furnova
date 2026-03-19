0647f03aa37222d05bcb00d739bdfa3c2a85df5d
Abdulloh2505 <daikarnon.5002@gmail.com>
2026-03-19 21:28:06 +0900

    feat: develop paswords Hashing

--- apps/furnova-api/src/components/member/member.service.ts
+++ apps/furnova-api/src/components/member/member.service.ts
@@ -5,13 +5,21 @@ import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class MemberService {
 
-    constructor(@InjectModel("Member") private readonly memberModel: Model<Member>){}
-
+    constructor(
+        @InjectModel('Member') private readonly memberModel: Model<Member>,
+       // @InjectModel('Member') private readonly followModel: Model<Follower | Following>,
+        private authService: AuthService,
+       // private viewService: ViewService,
+       // private likeService: LikeService,
+    ) { }
     public async signup(input: MemberInput): Promise<Member> {
+     input.memberPassword = await this.authService.hashPassword(input.memberPassword);
+
       
          try {
             const result = await this.memberModel.create(input);//skima
@@ -33,7 +41,7 @@ export class MemberService {
         }
 
         // TODO: Compear Pasword
-        const isMatch = memberPassword === response.memberPassword;
+        const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword);
         if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
        return response}
 
